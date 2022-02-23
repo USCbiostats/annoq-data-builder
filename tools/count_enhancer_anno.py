4a7949b8217e348417c5c8394149a22ff073c1fa
tmushayahama <tmushayahama@gmail.com>
2022-02-22 21:25:57 -0800

    Added the map for nice output script

--- tools/count_enhancer_anno.py
+++ tools/count_enhancer_anno.py
@@ -1,17 +1,17 @@
-
 import argparse
 from os import path as ospath
 
-col_names = ["assay",
-             "enhancer",
-             "GO_molecular_function_complete_list_id",
-             "GO_biological_process_complete_list_id",
-             "GO_cellular_component_complete_list_id",
-             "PANTHER_GO_SLIM_molecular_function_list_id",
-             "PANTHER_GO_SLIM_biological_process_list_id",
-             "PANTHER_GO_SLIM_cellular_component_list_id",
-             "REACTOME_pathway_list_id",
-             "PANTHER_pathway_list_id"]
+col_names = ["enhancer_linked_genes",
+             "enhancer_linked_assay",
+             "enhancer_linked_enhancer",
+             "enhancer_linked_GO_molecular_function_complete_list_id",
+             "enhancer_linked_GO_biological_process_complete_list_id",
+             "enhancer_linked_GO_cellular_component_complete_list_id",
+             "enhancer_linked_PANTHER_GO_SLIM_molecular_function_list_id",
+             "enhancer_linked_PANTHER_GO_SLIM_biological_process_list_id",
+             "enhancer_linked_PANTHER_GO_SLIM_cellular_component_list_id",
+             "enhancer_linked_REACTOME_pathway_list_id",
+             "enhancer_linked_PANTHER_pathway_list_id"]
 
 counts = dict()
 
@@ -22,6 +22,7 @@ def main():
 
     annotation_file = ospath.splitext(ospath.basename(vcf_path))[0]
 
+    counts['file'] = annotation_file
     counts['total'] = 0
 
     count_rows(vcf_path, col_names)
@@ -38,10 +39,12 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def is_annotated(annotation, k):
-    print(annotation[k])
-    if annotation[k] is not None or annotation[k] != "":
-        counts[k] += 1
+def is_annotated(annotation, d):
+    if annotation[d[1]] is not None and annotation[d[1]] != "" and len(annotation[d[1]].strip()) > 2:
+        counts[d[0]] += 1
+        return True
+    else:
+        return False
 
 
 def count_rows(filepath, col_names):
@@ -49,8 +52,10 @@ def count_rows(filepath, col_names):
     with open(filepath) as fp:
         row = fp.readline().rstrip()
         col_indices = get_column_indices(row, col_names)
-        for col_index in col_indices:
-            counts[col_index] = 0
+
+        for col_name in col_names:
+            counts[col_name] = 0
+
         while row:
             row = fp.readline().rstrip()
             if row:
@@ -59,17 +64,21 @@ def count_rows(filepath, col_names):
 
 def get_column_indices(row, col_names):
     line = row.rstrip().split("\t")
-    col_indices = [line.index(f'enhancer_linked_{col_name}')
-                   for col_name in col_names]
+    col_indices = {col_name: line.index(col_name)
+                   for col_name in col_names}
 
     return col_indices
 
 
 def count_row(row, col_indices):
     line = row.rstrip().split("\t")
+    annotated = False
 
-    for col_index in col_indices:
-        is_annotated(line, col_index)
+    for col_index in col_indices.items():
+        annotated |= is_annotated(line, col_index)
+
+    if annotated:
+        counts['total'] += 1
 
 
 if __name__ == "__main__":
